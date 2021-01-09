194c0a5253da317663015fb486576804e7c7b481
Veli Yavuz <58310814+11yavuz11@users.noreply.github.com>
2021-01-09 09:44:30 -0500

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,1 +1,24 @@
 
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+os.system("sudo apt-get install figlet")
+os.system("clear")
+os.system("figlet Mac Changer Auto")
+
+def Mac_Changer():
+    second = int(input("Please enter mac replacement time: "))
+    interface=input("Interface to change: ")
+    while True:
+        os.system("sudo macchanger -r "+interface)
+        time.sleep(second)
+
+
+
+Mac_Changer()
+
+
+
